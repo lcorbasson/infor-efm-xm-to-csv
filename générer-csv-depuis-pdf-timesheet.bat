6ea442b58039d4332fa95122ba3047168d88202c
Loïc CORBASSON <loic.corbasson@gmail.com>
2016-02-02 20:22:13 +0100

    Change .bat encoding to cp850

--- générer-csv-depuis-pdf-timesheet.bat
+++ générer-csv-depuis-pdf-timesheet.bat
@@ -1,2 +1,2 @@
 @echo off
-C:\Cygwin\bin\mintty.exe -t "%~nx0 (%~dp0)" -e /bin/bash -l -c """$(cygpath ""%~dpn0.sh"")""; echo 'Appuyez sur Entrée pour continuer...'; read a"
+C:\Cygwin\bin\mintty.exe -t "%~nx0 (%~dp0)" -e /bin/bash -l -c """$(cygpath ""%~dpn0.sh"")""; echo 'Appuyez sur Entr‚e pour continuer...'; read a"
